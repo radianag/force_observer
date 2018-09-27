dac5fb2cd032bac3b922fb298503566bb1e50e2d
radianag <ragondokaryono@wpi.edu>
2018-09-26 21:15:42 -0400

    fixed_working

--- scripts/calibrate_imu.py
+++ scripts/calibrate_imu.py
@@ -7,7 +7,7 @@
 import rospy
 import numpy as np
 import tf
-
+import csv
 import dvrk
 #import rospkg
 import std_msgs.msg
@@ -36,7 +36,10 @@ class Imu:
         self.state = 0
 
     def callback(self, msg):
-        self.data = msg.data
+        scale = 8.0 * 9.81 / 32767.0 
+        self.data = np.array(msg.data, dtype=float)
+        self.data[1:] = self.data[1:] *scale
+        
         #print(self.data)
 
     def set_data(self, R1):
@@ -44,9 +47,11 @@ class Imu:
         if self.avg_cnt == 0:
             self.x_array[0] = x
             self.y_array[0] = np.array([self.data[1], self.data[2], self.data[3]])
+            #print(self.y_array)
         else:
             self.x_array = np.concatenate((self.x_array, [x]), axis=0)
             self.y_array = np.concatenate((self.y_array, [[self.data[1], self.data[2], self.data[3]]]), axis=0)
+        #print("data", self.y_array)
         #print(self.x_array)
         self.avg_cnt += 1
 
@@ -58,8 +63,9 @@ class Imu:
                 X = R[i]
             else:
                 X = np.concatenate((X, R[i]), axis=1)
-
+        X = np.concatenate((X,np.eye(3)), axis=1)
         return X
+
     def concatenate_Xb(self, X, y):
         if np.sum(self.Xb) == 0:
             self.Xb = X
@@ -68,32 +74,40 @@ class Imu:
             self.Xb = np.concatenate((self.Xb, X), axis=0)
             self.Yb = np.concatenate((self.Yb, y))
         print("XB" , np.shape(self.Xb), np.shape(self.Yb))
-        print(self.Yb)
+        print("Yb", self.Yb)
 
     def get_avg(self):
         if self.avg_cnt > self.avg_data_pts:
-            avg_x = np.sum(self.x_array, axis=0)
-            avg_y = np.sum(self.y_array, axis=0)
-            print("avg", self.avg_x)
+            avg_x = np.sum(self.x_array, axis=0)/self.avg_cnt
+            avg_y = np.sum(self.y_array, axis=0)/self.avg_cnt
+            #print("avgcnt", self.avg_cnt)
+            #print("len", np.shape(self.y_array))
 
             self.state += 1
             self.avg_cnt = 0
             self.x_array = np.zeros([1, 3])
+            self.y_array = np.zeros([1, 3])
             X = self.make_X(avg_x)
             self.concatenate_Xb(X, avg_y)
 
 
     def solve_LS(self):
-    	self.G = np.matmul(np.linalg.pinv(self.Xb),self.Yb)
-    	print(self.G)
+        self.G = np.matmul(np.linalg.pinv(self.Xb),self.Yb)
+        print("offset values", self.G)
+        self.make_csv()
+
+    def make_csv(self):
+        with open('imu_offsets.csv', 'wb') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_NONE)
+            wr.writerow(self.G)
 
 if __name__ == '__main__':
     rospy.init_node('calibration', anonymous=True)
 
     #Settings
     ctrl_rate = 100       # [Hz]
-    stabil_ts = 1.5 #[sec]
-	
+    stabil_ts = 2 #[sec]
+    
     q1_num_pts = 4
     q2_num_pts = 4
 
@@ -108,6 +122,7 @@ if __name__ == '__main__':
     num_points = q1_num_pts*q2_num_pts
 
     p = dvrk.psm('PSM1')
+    p.home()
     imu = Imu(ctrl_rate*stabil_ts)
 
     # a = p.get_current_joint_position()[0:3]
@@ -116,37 +131,37 @@ if __name__ == '__main__':
 
     yaw = 1
     pitch = 1.54
-    R1 = tf.transformations.euler_matrix(yaw, pitch, 0, 'syxz')
-    imu.set_data(R1)
-    X = imu.make_X(imu.x_array[0])
+    #R1 = tf.transformations.euler_matrix(yaw, pitch, 0, 'syxz')
+    
+    #imu.set_data(R1)
+    #X = imu.make_X(imu.x_array[0])
 
-    imu.concatenate_Xb(X, [1, 2, 3])
-    imu.concatenate_Xb(X, [1, 2, 3])
-    imu.concatenate_Xb(X, [1, 2, 3])
+    #imu.concatenate_Xb(X, [1, 2, 3])
+    #imu.concatenate_Xb(X, [1, 2, 3])
+    #imu.concatenate_Xb(X, [1, 2, 3])
 
     state1 = np.linspace(0,q1_num_pts-1,q1_num_pts, dtype=int)
     state2 = np.linspace(0,q2_num_pts-1,q2_num_pts, dtype=int)
 
     flat_state= [(0,0)]* num_points
     for i in range(q1_num_pts):
-    	for j in range(q2_num_pts):
-    		flat_state[(i)*q2_num_pts+j] = (state1[i], state2[j])
-    print(flat_state)
+        for j in range(q2_num_pts):
+            flat_state[(i)*q2_num_pts+j] = (state1[i], state2[j])
+    print(traj_q1)
 
-    
+    a = [0, 0]
     while (imu.state < num_points ) and not rospy.is_shutdown():
-		# print(traj_q1[flat_state[imu.state][0]], traj_q2[flat_state[imu.state][0]])
-		# imu.state +=1
-		if imu.avg_cnt = 0:
-    		p.move_joint_some(np.array([traj_q1[flat_state[imu.state][0]], traj_q2[flat_state[imu.state][1]], q3_pos]), np.array([0, 1, 2]))
-  			r.sleep(1)
-    	a = p.get_current_joint_position()[0:2]
-    	yaw = a[0]
-    	pitch = a[1]
-    	R1 = tf.transformations.euler_matrix(yaw, pitch, 0, 'syxz')
-   		imu.set_data(R1)
-    	imu.get_avg()
- 		r.sleep()
+
+        if imu.avg_cnt == 0:
+            p.move_joint_some(np.array([traj_q1[flat_state[imu.state][0]], traj_q2[flat_state[imu.state][1]], q3_pos]),np.array([0, 1, 2]))
+            rospy.sleep(5)
+        a = p.get_current_joint_position()[0:2]
+        yaw = a[0]
+        pitch = a[1]
+        R1 = tf.transformations.euler_matrix(yaw, pitch, 0, 'syxz')
+        imu.set_data(R1)
+        imu.get_avg()
+        r.sleep()
 
     imu.solve_LS()
 
