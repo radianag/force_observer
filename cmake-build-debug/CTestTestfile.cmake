b229cd885b7761aa9ae6ca3b4b508ed313256817
Radian Azhar <radiang91@gmail.com>
2018-09-27 10:07:46 -0400

    delete

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radian/radian_ws/src/force_observer
-# Build directory: /home/radian/radian_ws/src/force_observer/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
